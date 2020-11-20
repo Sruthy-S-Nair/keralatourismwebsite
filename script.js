0097df62b6ef61d02f155fdf676c3063b926f340
sruthy <sruthysn16@gmail.com>
2020-11-20 21:32:12 +0530

    first commit

--- script.js
+++ script.js
@@ -35,15 +35,15 @@ let password = document.getElementById("pwd");
 let errorpwd = document.getElementById("errorpwd"); 
 function pwd()
 {
- let regexp=  /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{8,15}$/;
- if(regexp.test (password.value)){
-    errorpwd.innerHTML="valid";
-    errorpwd.style.color="green";
+   let regexp=  /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{8,15}$/;
+   if(regexp.test (password.value)){
+       errorpwd.innerHTML="valid";
+       errorpwd.style.color="green";
     return true;
-}
-else{
-    errorpwd.innerHTML ="invalid";
-    errorpwd.style.color = "red";
+    }
+    else{
+      errorpwd.innerHTML ="invalid";
+      errorpwd.style.color = "red";
     return false;
-}
+    }
 }
